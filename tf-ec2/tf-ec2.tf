acb1d2ea4319e88060231b65815064a05d73caf6
Tarek Ferjaoui <t.ferjaoui@melijoe.com>
2020-01-23 18:43:43 +0100

    first ec2 terraform

--- tf-ec2/tf-ec2.tf
+++ tf-ec2/tf-ec2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
+  profile = "default"
+  region  = "eu-west-3"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-0581b2fffa0f0453b"
   instance_type = "t2.micro"
 }
