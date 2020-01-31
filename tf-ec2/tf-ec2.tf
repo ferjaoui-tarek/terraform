c28735045d74a05cb58991dcd4c66c8261a552e5
Tarek Ferjaoui <t.ferjaoui@melijoe.com>
2020-01-31 17:13:54 +0100

    add some parameters in the main file

--- tf-ec2/tf-ec2.tf
+++ tf-ec2/tf-ec2.tf
@@ -1,9 +1,30 @@
+variable "region" {
+  default = "eu-west-3"
+}
 provider "aws" {
   profile = "default"
-  region  = "eu-west-3"
+  region  = var.region
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0581b2fffa0f0453b"
+#resource "aws_eip" "ip" {
+#  vpc      = true
+#  instance = aws_instance.myFirstTerraform.id
+#}
+
+resource "aws_instance" "myFirstTerraform" {
+  ami           = "ami-007fae589fdf6e955"
   instance_type = "t2.micro"
+  tags = {
+    Name = "MyFirstTerraformInstance"
+  }
+
+  depends_on = [aws_s3_bucket.myFirstTerraform]
+}
+
+resource "aws_s3_bucket" "myFirstTerraform" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "my-first-terraform-bucket-trx"
+  acl    = "private"
 }
