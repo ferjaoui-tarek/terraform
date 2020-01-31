270eb528d6e088e05d074193ff948ab78c897577
Tarek Ferjaoui <t.ferjaoui@melijoe.com>
2020-01-31 17:39:01 +0100

    add variables file

--- tf-ec2/vars.tf
+++ tf-ec2/vars.tf
@@ -1,1 +1,11 @@
-region = "eu-west-3"
+variable "region" {
+  default = "eu-west-3"
+}
+
+variable "ami-id" {
+  default = "ami-007fae589fdf6e955"
+}
+
+variable "ec2-size" {
+  default = "t2.micro"
+}
