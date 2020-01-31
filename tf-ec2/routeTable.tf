5d95d56596c416c010f761752962ce74c643db7b
Tarek Ferjaoui <t.ferjaoui@melijoe.com>
2020-01-31 23:26:34 +0100

    global files

--- tf-ec2/routeTable.tf
+++ tf-ec2/routeTable.tf
@@ -3,4 +3,7 @@ resource "aws_route" "internet_access" {
   route_table_id         = aws_vpc.myFirstTerraformVPC.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.myFirstTerraformIGW.id
+  tags = {
+    Name = "internet_access"
+  }
 }
--- tf-ec2/vpc.tf
+++ tf-ec2/vpc.tf
@@ -1,4 +1,7 @@
 # Create a VPC to launch our instances into
 resource "aws_vpc" "myFirstTerraformVPC" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "myFirstTerraformVPC"
+  }
 }
