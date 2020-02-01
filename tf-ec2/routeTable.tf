a649410e3655610a37cc45cbf48ea018cd5fa6a5
Tarek Ferjaoui <t.ferjaoui@melijoe.com>
2020-02-01 01:01:35 +0100

    global config

--- tf-ec2/routeTable.tf
+++ tf-ec2/routeTable.tf
@@ -3,7 +3,4 @@ resource "aws_route" "internet_access" {
   route_table_id         = aws_vpc.myFirstTerraformVPC.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.myFirstTerraformIGW.id
-  tags = {
-    Name = "internet_access"
-  }
 }
--- tf-ec2/sg.tf
+++ tf-ec2/sg.tf
@@ -18,7 +18,8 @@ resource "aws_security_group" "myFirstTerraformSG" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+#    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # outbound internet access
